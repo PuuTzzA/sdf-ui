81c978b145699a69b34bf5a0075a86daf50c1a47
PuuTzzA <putzerthomas03@gmail.com>
2026-09-04 00:06:12 +0200

    fix clear

--- src/scripts/sdf-canvas.js
+++ src/scripts/sdf-canvas.js
@@ -969,7 +969,7 @@ class SdfCanvas {
         gl.disable(gl.SCISSOR_TEST);
         gl.enable(gl.DEPTH_TEST);
         gl.depthFunc(gl.LEQUAL);
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearColor(0.0, 0.0, 0.0, 0.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     }
 
